fe50348d70079ba35ba22627fde3ec848a37b6fb
Sean Kembo <kembosean7@gmail.com>
2025-01-10 02:47:08 +0200

    initilaize database

--- firebase.py
+++ firebase.py
@@ -5,3 +5,5 @@ from firebase_admin import credentials, auth, firestore
 credentials_file = credentials.Certificate(r"C:\Users\User\Downloads\skillssync-ebaa9-firebase-adminsdk-ap0rz-3ed4e67283.json")
 firebase_admin.initialize_app(credentials_file)
 
+db = firestore.client()
+
