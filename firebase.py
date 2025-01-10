ad3a0fe64766799ca597d1edd75765d5e4dfa191
Sean Kembo <kembosean7@gmail.com>
2025-01-10 02:46:20 +0200

    initialize firebase with credentials

---
+++ firebase.py
@@ -0,0 +1,7 @@
+import firebase_admin
+from firebase_admin import credentials, auth, firestore
+
+
+credentials_file = credentials.Certificate(r"C:\Users\User\Downloads\skillssync-ebaa9-firebase-adminsdk-ap0rz-3ed4e67283.json")
+firebase_admin.initialize_app(credentials_file)
+
