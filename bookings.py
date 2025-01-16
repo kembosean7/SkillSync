97aece521822a9bcaa5baa8bca5544390afe6320
Sean Kembo <kembosean7@gmail.com>
2025-01-16 17:16:57 +0200

    func that makes a booking between peer and mentor

--- bookings.py
+++ bookings.py
@@ -24,3 +24,29 @@ def get_mentors():
 
 
 
+def book_mentor_session(user_email, mentor_email, session_time):
+    try:
+        mentor_ref = firebase_client().collection('users')
+        mentor = mentor_ref.where("email", "==", mentor_email).get()
+        
+        if mentor:
+            mentor_data = mentor[0].to_dict()
+            mentor_name = mentor_data['email']
+            
+            
+            session_ref = firebase_client().collection('sessions').add({
+                'user_email': user_email,
+                'mentor_id': mentor_email,
+                'mentor_name': mentor_name,
+                'session_time': session_time,
+                'status': 'scheduled'
+            })
+            print(f"Session scheduled with {mentor_name} at {session_time}.")
+        else:
+            print("Mentor not found.")
+    
+    except Exception as e:
+        print(f"Error occurred: {e}")
+
+get_mentors()
+book_mentor_session("kembosean7@gmail.com", "davidmalan@gmail.com","14:00")
